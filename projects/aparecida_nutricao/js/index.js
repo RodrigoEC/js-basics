dd29283a17a989a309581c1aa2afd683326531fd
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-04 13:42:56 -0300

    Crição de um script js que calcula o IMC de cada linha da tabela da aparecida nutrição

--- projects/aparecida_nutricao/js/index.js
+++ projects/aparecida_nutricao/js/index.js
@@ -1,2 +1,21 @@
+console.log("Fui carregado de um arquivo externo")
+
 var mainTitle = document.querySelector("#main-title");
 mainTitle.textContent = "I hereee";
+
+// Calcular o IMC
+
+function calculaIMC(paciente) {
+    peso = parseInt(paciente.querySelector(".info-peso").textContent)
+    altura = parseInt(paciente.querySelector(".info-altura").textContent)
+    return peso / Math.pow(altura, 2)
+}
+
+const pacientes = document.querySelectorAll(".paciente")
+// console.log(pacientes)
+
+for (paciente of pacientes) {
+    imcPaciente = paciente.querySelector(".info-imc")
+    imcPaciente.textContent = calculaIMC(paciente)   
+    // console.log(calculaIMC(paciente))
+}
